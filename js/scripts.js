e0747bdf77193df91ffdd55b44f952a347f9d0b3
xutku <122744396+xutku@users.noreply.github.com>
2023-02-07 18:52:50 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
-/*!
-* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
+
 // Scripts
 // 
 
